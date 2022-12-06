93f90da5ffcb863646eea1e2d7767526781152ea
twissy22 <adamdtwiss@hotmail.com>
2022-12-06 11:57:06 +0000

    added get reviews

---
+++ src/reviews.js
@@ -0,0 +1,37 @@
+import { useEffect, useState, useContext} from "react";
+import {getReviews} from "./api"
+const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getReviews().then((reviews)=>{
+            setReviews(reviews.reviews)
+            setLoading(false)
+        })
+    },[]);
+
+   return loading ? (<p>...Loading</p>) 
+   : ( <ul>
+        {reviews.map((review)=>{
+            console.log(review)
+        return(
+<li key={review.review_id}className="list">
+<section className="listing">
+<img src={review.review_img_url} className="pic"></img>
+<article classname="text">
+<h3 className="reviewstitle">Title : {review.title}</h3>
+<button className="viewmoreinfo">View Review</button>
+</article>
+</section>
+</li>
+
+
+        )}
+         
+        )}
+ </ul>
+ )
+        }
+export default Reviews;
